e3ca8addf82274abadab2019522f36401914ea03
Kim Ryeonwoo <kimlw90@naver.com>
2018-10-29 12:48:17 +0900

    Reverted changes

--- ophsig/src/ophSigPU.h
+++ ophsig/src/ophSigPU.h
@@ -10,7 +10,11 @@
 * @addtogroup PU
 //@{
 * @detail
-
+This module contains methods related to phase unwrapping.
+Phase unwrapping algorithm unwraps the 2-pi wrapped phase data to obtain continuous phase distribution.
+This module uses the Goldstein's branch cut algorithm. R.M.Goldstein, H.A. Zebker, and C.L. Werner, "Satellite radar interferometry: two-dimensional phase unwrapping," Radio Science, vol. 23, no. 4, pp. 713-720, 1988.
+This module was written with slight modifications based on 
+https://kr.mathworks.com/matlabcentral/fileexchange/22504-2d-phase-unwrapping-algorithms
 */
 //! @} PU
 
@@ -24,11 +28,35 @@ class SIG_DLL ophSigPU : public ophSig
 public:
 	explicit ophSigPU(void);
 
+	/**
+	* @brief Set parameters for Goldstein branchcut algorithm 
+	* @param maxBoxRadius : maximum box radius for neighboring residue search
+	*/
 	bool setPUparam(int maxBoxRadius);
+
+	/**
+	* @brief Load original wrapped phase data
+	* @param fname : image file name of wrapped phase data
+	* @param bitpixel : the number of bits per pixel in the image file
+	*/
 	bool loadPhaseOriginal(const char *fname, int bitpixel);
 	bool loadPhaseOriginal(void);
+
+	/**
+	* @brief Run phase unwrapping algorithm
+	*/
 	bool runPU(void);
+
+	/**
+	* @brief Save the unwrapped phase data to image file
+	* @param fname : image file name where the unwrapped phase data will be stored
+	*/
 	bool savePhaseUnwrapped(const char *fname);
+
+	/**
+	* @brief Read configure file
+	* @param fname : configure file name
+	*/
 	bool readConfig(const char * fname);
 
 protected:
